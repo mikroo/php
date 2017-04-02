43f81805818987ccf8f7307b42100a6850abc595
Chancy Ntimba <ch.ntimba@bluewin.ch>
2017-04-02 13:17:32 +0100

    les variables

---
+++ 01_intro/les_balises_php.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Ma page web</title>
+</head>
+<body>
+	<h1>Ma page web</h1>
+	<p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?> </p>
+
+	<?php  
+		echo "Ma première instruction.";
+		echo "Ma deuxième instruction.";
+		ecxho "Et en voila une troisième !";
+	?>
+</body>
+</html>
---
+++ 02_basics/variables.php
@@ -0,0 +1,10 @@
+<?php  
+// Affecter une valeur à une variable
+$age_du_visiteur = 17; // La variable est créée et vaut 17 
+$age_du_visiteur = 23; // La variable est modifiée et vaut 23
+$age_du_visiteur = 55; // La variable est modifiée et vaut 55
+
+// Une variable vide avec NULL
+$pas_de_valeur = NULL;
+// Afficher le contenu d'une variable
+echo $age_du_visiteur;
